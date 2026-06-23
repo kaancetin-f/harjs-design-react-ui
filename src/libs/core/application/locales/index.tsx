20248970c891b20c469af5b428131919366dd107
kaan.cetin <kaan.cetin@trt.net.tr>
2026-06-23 22:45:10 +0300

    Notification çevirileri çıkartıldı.

--- src/libs/core/application/locales/index.tsx
+++
@@ -1,5 +0,0 @@
-import INotificationLocale from "./notification/INotificationLocale";
-import NotificationTR from "./notification/tr";
-import NotificationEN from "./notification/en";
-
-export { INotificationLocale, NotificationTR, NotificationEN };
